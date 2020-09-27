0b1c722d9759eeb288f38e8edd3939d64a525ac6
galloafranco <galloafranco@gmail.com>
2020-09-27 18:14:08 -0300

    * Se implementa provider en nuestro controller con el manejo de responses necesario.

--- src/api/messages/messages.controller.ts
+++ src/api/messages/messages.controller.ts
@@ -1,26 +1,51 @@
-import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Res } from '@nestjs/common';
 import { MessageDto } from '../../responses/message-dto.interface';
+import { MessageService } from '../../services/message/message.service';
 
-@Controller('messages')
+@Controller('message')
 export class MessagesController {
 
+
+  constructor(private readonly messageService: MessageService) {
+  }
+
   @Post()
-  createMessage(@Body() messageDto: MessageDto) {
-    return 'mensaje creado';
+  createMessage(@Body() messageDto: MessageDto, @Res() response) {
+    return this.messageService.create(messageDto).then( (res) => {
+      response.status(HttpStatus.CREATED).json(res);
+      console.log('create success'); // In future will use Logger
+    }).catch( () => {
+      response.status(HttpStatus.FORBIDDEN).json({ message: 'Error al crear mensaje' })
+    });
   }
 
   @Get()
-  getAll() {
-    return 'lista de mensajes';
+  getAll(@Res() response) {
+    return this.messageService.getAll().then((res) => {
+      response.status(HttpStatus.OK).json(res);
+      console.log('get success') // In future will use Logger
+    }).catch( () => {
+      response.status(HttpStatus.FORBIDDEN).json({ message: 'Error al obtener la lista de mensajes' })
+    });
   }
 
   @Put(':id')
-  updateMessage(@Body() messageDto: MessageDto) {
-    return 'mensaje actualizado';
+  updateMessage(@Body() messageDto: MessageDto, @Res() response, @Param('id') idMessage: number) {
+    return this.messageService.update(messageDto, idMessage).then((res) => {
+      response.status(HttpStatus.OK).json(res);
+      console.log('update success'); // In future will use Logger
+    }).catch(
+      response.status(HttpStatus.FORBIDDEN).json({message:'Error al actualizar mensaje'})
+    );
   }
 
   @Delete(':id')
-  deleteMessage() {
-    'mensaje eliminado';
+  deleteMessage(@Param('id') idMessage: number, @Res() response) {
+    return this.messageService.delete(idMessage).then((res) => {
+      response.status(HttpStatus.OK).json(res);
+      console.log('delete success'); // In future will use Logger
+    }).catch( () => {
+      response.status(HttpStatus.FORBIDDEN).json({ message: 'Error al actualizar mensaje' })
+    })
   }
 }
