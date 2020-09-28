ad62277eb7a398c4e9777debd07ac6f0690b2185
galloafranco <galloafranco@gmail.com>
2020-09-28 20:54:01 -0300

    * Agregamos y configuramos swagger basico al proyecto en la direccion /message/docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Messages')
+    .setDescription('Simple CRUD for messages')
+    .setVersion('1.0.0')
+    .addTag('Messages')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('/message/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
