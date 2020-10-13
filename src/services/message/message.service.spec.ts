436f109a72f804c16bc0c6239e0d9356be319dec
galloafranco <galloafranco@gmail.com>
2020-10-12 23:25:08 -0300

    * Fix message.service.spec.ts

--- src/services/message/message.service.spec.ts
+++ src/services/message/message.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MessageService } from './message.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { MessageEntity } from '../../models/message/message.entity';
 
 describe('MessageService', () => {
   let service: MessageService;
+  const messageRepository = {};
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MessageService],
+      providers: [MessageService,
+        {provide: getRepositoryToken(MessageEntity), useValue: messageRepository}],
     }).compile();
 
     service = module.get<MessageService>(MessageService);
